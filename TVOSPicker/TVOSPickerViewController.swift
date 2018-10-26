cf1cf414079d68f08ce443088319a41149b2c749
Milos Mokic <milosmokic@gmail.com>
2018-10-26 14:01:09 +0200

    Updated to Swift 4.2

--- TVOSPicker/TVOSPickerViewController.swift
+++ TVOSPicker/TVOSPickerViewController.swift
@@ -258,7 +258,7 @@ public class TVOSPickerViewController: UIViewController, UICollectionViewDelegat
     let item = dataSource[indexPath.item]
     let itemWidth = NSAttributedString(
       string: item,
-      attributes: [NSAttributedStringKey.font: UIFont.preferredFont(forTextStyle: .body)]
+      attributes: [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .body)]
     ).boundingRect(
         with: CGSize(width: .max, height: .max),
         options: .usesDeviceMetrics,
